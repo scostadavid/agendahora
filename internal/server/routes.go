f13b3b6b2438fa7bd44dbb709ad01829e4138fe7
scostadavid <scostadavid@proton.me>
2024-06-22 12:44:02 -0300

    feat: basic landing page

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -13,14 +13,16 @@ import (
 func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
 
+	// api json endpoints
 	mux.HandleFunc("GET /hello-world", s.HelloWorldHandler)
 	mux.HandleFunc("GET /health", s.healthHandler)
+	mux.HandleFunc("POST /hello", web.HelloWebHandler)
 
 	fileServer := http.FileServer(http.FS(web.Files))
 
+	// static pages
 	mux.Handle("GET /assets/", fileServer)
-	mux.Handle("GET /web", templ.Handler(web.HelloForm()))
-	mux.HandleFunc("POST /hello", web.HelloWebHandler)
+	mux.Handle("GET /", templ.Handler(web.LandingPage()))
 
 	return mux
 }
